4de4ab175202601d4e32f6db48b0e4cdfa16fbda
Lamar Greene <github@lamargreene.com>
2017-06-11 10:09:07 -0600

    Section 6 complete
    This brings us to the end of section 6. The API now uses SQLAlchemy all
    the way through, and the database will be created if it does not exist.
    (It still uses SQLite, creating a file is diff than accessing stand
    alone DB). Also added a store resource and endpoint.

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,82 +1,65 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
 
 class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+		type=float,
+		required=True,
+		help="This field cannot be left blank!"
+	)
+	parser.add_argument('store_id',
+		type=int,
+		required=True,
+		help="Every item needs a store id."
+	)
 
-    @jwt_required()
-    def get(self, name):
-        item = ItemModel.find_by_name(name)
-        if item:
-            return item.json()
-        return {'message': 'Item not found'}, 404
-
-
-    def post(self, name):
-        if ItemModel.find_by_name(name):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        data = Item.parser.parse_args()
-
-        item = ItemModel(name, data['price'])
-
-        try:
-            item.insert()
-        except:
-            return {'message': 'An error occurred inserting the item.'}, 500
-
-        return item.json(), 201
+	@jwt_required()
+	def get(self, name):
+		item = ItemModel.find_by_name(name)
+		if item:
+			return item.json()
+		return {'message': 'Item not found'}, 404
 
 
-    def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+	def post(self, name):
+		if ItemModel.find_by_name(name):
+			return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
+		data = Item.parser.parse_args()
 
-        connection.commit()
-        connection.close()
+		item = ItemModel(name, **data) # or --> ItemModel(name, data['price'], data['store_id'])
 
-        return {'message': 'Item deleted'}
+		try:
+			item.save_to_db()
+		except:
+			return {'message': 'An error occurred inserting the item.'}, 500
+
+		return item.json(), 201
 
 
-    def put(self, name):
-        data = Item.parser.parse_args()
+	def delete(self, name):
+		item = ItemModel.find_by_name(name)
+		if item:
+			item.delete_from_db()
+		return {'message': 'Item deleted'}
 
-        item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
 
-        if item is None:
-            try:
-            	updated_item.insert()
-            except:
-                return {'message': 'An error occurred inserting the item.'}, 500
-        else:
-            try:
-                updated_item.update()
-            except:
-                return {'message': 'An error occurred updating the item.'}, 500
-        return updated_item.json()
+	def put(self, name):
+		data = Item.parser.parse_args()
+
+		item = ItemModel.find_by_name(name)
+
+		if item is None:
+			item = ItemModel(name, **data) # or --> ItemModel(name, data['price'], data['store_id'])
+		else:
+			item.price = data['price']
+			item.store_id = data['store_id']
+		item.save_to_db()
+		return item.json()
 
 
 class ItemList(Resource):
-    def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        connection.close()
-
-        return {'items': items}
+	def get(self):
+		return {'items': [item.json() for item in ItemModel.query.all()]}  # or --> {'items': list(map(lambda x:  x.json(), ItemModel.query.all()))}
--- code/resources/user.py
+++ code/resources/user.py
@@ -5,30 +5,24 @@ from models.user import UserModel
 
 class UserRegister(Resource):
 
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type = str,
-                        required = True,
-                        help = "This field cannot be blank.")
+	parser = reqparse.RequestParser()
+	parser.add_argument('username',
+						type = str,
+						required = True,
+						help = "This field cannot be blank.")
 
-    parser.add_argument('password',
-                        type = str,
-                        required = True,
-                        help = "This field cannot be blank.")
+	parser.add_argument('password',
+						type = str,
+						required = True,
+						help = "This field cannot be blank.")
 
-    def post(self):
-        data = UserRegister.parser.parse_args()
+	def post(self):
+		data = UserRegister.parser.parse_args()
 
-        if UserModel.find_by_username(data['username']):
-            return {"message": "A user with that username already exists"}, 400
+		if UserModel.find_by_username(data['username']):
+			return {"message": "A user with that username already exists"}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+		user = UserModel(**data) # same as --> UserModel(data['username'], data['password'])
+		user.save_to_db()
 
-        query = "INSERT INTO users VALUES(NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
-
-        return {"message": "User created successfully."}, 201
+		return {"message": "User created successfully."}, 201
